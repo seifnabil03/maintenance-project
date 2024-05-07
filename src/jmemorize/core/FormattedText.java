9d35f99771b7e38413407c5708deebc04cee4b4a
omarKhaled74 <150850927+omarKhaled74@users.noreply.github.com>
2024-05-07 16:36:34 +0300

    error

--- src/jmemorize/core/FormattedText.java
+++ src/jmemorize/core/FormattedText.java
@@ -332,7 +332,7 @@ public class FormattedText implements Cloneable
         
         return sb.toString();
     }
-    
+    //seif
     private static String escape(String text)
     {
         return text.replaceALL("<", "&lt;").replaceALL(">", "&gt;");
